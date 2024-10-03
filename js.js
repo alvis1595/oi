1896e3daa24fa36c7f8752c0eb94b49439444d5c
alvis1595 <39422675+alvis1595@users.noreply.github.com>
2024-10-03 15:17:13 -0500

    Create js.js

---
+++ js.js
@@ -0,0 +1,21 @@
+{
+  "servidor": "servidor1",
+  "ip": "192.168.1.1",
+  "tipo_servidor": "Linux",
+  "entorno_ci": "Desarrollo",
+  "pais": "España",
+  "aplicacion": "Aplicacion1",
+  "lenguaje": "Python",
+  "proveedor": "ProveedorX",
+  "desarrollo": "Desarrollo Interno",
+  "responsable": "Responsable1",
+  "rep": "Rep1",
+  "alojamiento": "AWS",
+  "esquema_de_continuidad": "Esquema1",
+  "estrategias_de_recuperacion_infra": "Infra1",
+  "estrategias_de_recuperacion_datos": "Datos1",
+  "tiempo_de_instalacion_servidor": "5 horas",
+  "tiempo_de_instalacion_aplicacion": "3 horas",
+  "joya_de_la_corona": "Sí",
+  "creador": "usuario1"
+}
